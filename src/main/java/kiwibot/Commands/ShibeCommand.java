787c7cd92ff909687fd32948632b0d1bed83cb54
Yi Zhang <artificialstyrofoam@gmail.com>
2021-06-26 11:09:05 -0700

    Make the command actually work

--- src/main/java/kiwibot/Commands/ShibeCommand.java
+++ src/main/java/kiwibot/Commands/ShibeCommand.java
@@ -24,13 +24,16 @@ public class ShibeCommand extends MasterCommand {
         switch (_args.get(0)) {
             case "doge":
             case "shibe":
-            Request request = new Request.Builder().url(url).build();
-            try {
-                Response res = client.newCall(request).execute();
-                String url = res.body().string().replace("([[\"])([\"\\]])", "");
-                EmbedBuilder embed = new EmbedBuilder();
-                embed.setTitle("Here's your funny yellow dog");
-                embed.setImage(url);
+                Request request = new Request.Builder().url(url).build();
+                try {
+                    Response res = client.newCall(request).execute();
+                    String dogimage = res.body().string();
+                    dogimage = dogimage.substring(2, dogimage.length()-2);
+                    EmbedBuilder embed = new EmbedBuilder();
+                    System.out.println(dogimage);
+                    embed.setTitle("Here's your funny yellow dog");
+                    embed.setImage(dogimage);
+                    _e.getChannel().sendMessage(embed.build()).queue();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
